8c25112677aa1c5468f3836ae68d5e59d038bf04
André Fajardo <117539643+andrefajardo@users.noreply.github.com>
2025-10-07 14:51:58 -0300

    Create:   exercise files
    Code of three basics exercicses in Python.
    <Resp: André Fajardo - REF #000>

---
+++ world-1/ex005-antecesorSucessor-PRINT-FORMAT.py
@@ -0,0 +1,4 @@
+num = int(input('Informe um número: '))
+antec = num - 1
+suces = num + 1
+print('O número {:.2f} tem como antecessor o valor {:.2f} e como sucessor o valor {:.2f}' .format(num, antec, suces))
---
+++ world-1/ex006-dobroTriploRaizQuadrada.py
@@ -0,0 +1,5 @@
+num = int(input('Informe um número: '))
+dobro = num * 2
+triplo = num * 3
+raiz = num ** (1/2)
+print('O número {:.2f} tem como dobro o valor {:.2f}, como triplo o valor {:.2f} e sua raiz quadrada é {:.2f}' .format(num, dobro, triplo, raiz))
